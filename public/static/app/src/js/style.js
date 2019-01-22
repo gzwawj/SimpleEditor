1723651a11f9a51f08711a9edbddadec193357b0
gzwawj <936588357@qq.com>
2019-01-22 17:41:19 +0800

    添加marked编辑器

--- public/static/app/src/js/style.js
+++ public/static/app/src/js/style.js
@@ -2,9 +2,9 @@ define(function (require, exports, module) {
     let cookie = require('./cookie')
     let config = {
         pro: "php",
-        db:"mysql",
-        edit:"ueditor",
-        bg:"d0d0d0"
+        db: "mysql",
+        edit: "ueditor",
+        bg: "d0d0d0"
     }
     $('input').click(function (e) {
         if (e.target.name == 'pro') {
@@ -15,11 +15,12 @@ define(function (require, exports, module) {
         }
         if (e.target.name == 'edit') {
             config.edit = e.target.value
+            editor(e.target.value)
         }
         if (e.target.name == 'bg') {
             config.bg = e.target.value
             $('body').css({
-                "background": "#"+e.target.value
+                "background": "#" + e.target.value
             })
         }
         cookie.setCookie('config', JSON.stringify(config))
@@ -31,8 +32,6 @@ define(function (require, exports, module) {
      * 3.默认样式
      */
     let init = function () {
-        //默认编辑器
-        UE.getEditor('editor');
         let conf = JSON.parse(cookie.getCookie('config'))
         config = conf ? conf : config
         $('input[type=radio]').each(function (e) {
@@ -44,14 +43,68 @@ define(function (require, exports, module) {
             }
             if (this.value == config.edit) {
                 $('input[value=' + this.value + ']').attr("checked", "checked");
+                //默认编辑器
+                editor(this.value)
             }
             if (this.value == config.bg) {
                 $('input[value=' + this.value + ']').attr("checked", "checked");
                 $('body').css({
-                    "background": "#"+this.value
+                    "background": "#" + this.value
                 })
             }
         })
+
+        $('#mdfile').change(function () {
+            let data = new FormData()
+            let md = document.getElementById('mdfile').files
+            data.append('md', md[0], md[0].name)
+
+            $.ajax({
+                url: "/server/php/index.php",
+                data: data,
+                type: "POST",
+                processData: false,
+                contentType: false,
+                success: function (e) {
+                    $('#codebox').empty()
+                    $('#codebox').append('<pre id="marked-content">' + e + '</pre>')
+                    let marked_id = 'marked-content'
+                    //获取文本
+                    let content = document.getElementById(marked_id).innerHTML;
+                    //正则替换标签
+                    content = content.replace(/(<div>|<br>|<\/div>|\s)*(<div>|<br>|<\/div>)/g, "\n");
+                    //marked转换
+                    document.getElementById(marked_id).innerHTML = marked(content);
+                    //代码高亮
+                    $('pre code').each(function (i, block) {
+                        hljs.highlightBlock(block);
+                    });
+                }
+            })
+        })
+    }
+    let editor = function (value) {
+        $('.editbox').empty()
+        if (value == 'ueditor') {
+            $('.editbox').append('<script id="editor" name="content" type="text/plain"></script>')
+
+            UE.getEditor('editor');
+        }
+        if (value == 'marked') {
+            $('.editbox').append('<div id="editor"><textarea style="display:none;"></textarea></div>')
+            $('.editbox').append('<link href="/public/editor/marked/css/editormd.min.css" rel="stylesheet">')
+            let editormd = require('../../../../editor/marked/src/editormd')
+            editormd("editor", {
+                path: "./public/editor/marked/lib/", // Autoload modules mode, codemirror, marked... dependents libs path
+                toolbarIcons: 'simple'
+            });
+        }
+        if (value == 'md') {
+            $('.editbox').append('<input id="mdfile" type="file"><div id="codebox"></div>')
+            $('.editbox').append('<link href="/public/editor/md/css/monokai_sublime.min.css" rel="stylesheet">')
+            $('.editbox').append('<script src="/public/editor/md/js/marked.min.js"></script>')
+            $('.editbox').append('<script src="/public/editor/md/js/highlight.min.js"></script>')
+        }
     }
     /**
      * 创建文章列表
@@ -77,7 +130,19 @@ define(function (require, exports, module) {
         //创建li数据列表
         document.getElementById('article_lst').innerHTML = str;
     }
-
+    /**
+     * 使用注释的方式添加代码块
+     */
+    Function.prototype.getMultiLine = function () {
+        var lines = new String(this);
+        lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));
+        return lines;
+    }
+    let addCode = function () {
+        /* 
+        js添加代码块测试，使用addCode.getMultiLine()
+        */
+    }
     module.exports = {
         init: init,
         datalist: datalist
