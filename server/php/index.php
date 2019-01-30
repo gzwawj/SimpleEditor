b32a9851759aeac5613190ec53cee3f97443989f
gzwawj <936588357@qq.com>
2019-01-30 17:38:07 +0800

    添加处理方法

--- server/php/index.php
+++ server/php/index.php
@@ -12,20 +12,109 @@ function __autoload($className)
         die;
     }
 }
+/**
+ * 文章列表
+ */
+function articleLst($model)
+{
+    $res = $model->getAllData('article');
+    if ($res) {
+        $data = array();
+        foreach ($res as $k => $v) {
+            $data[] = array(
+                "title" => scapehtml($v['title']),
+                "categories" => scapehtml($v['categories']),
+                "keywords" => scapehtml($v['keywords']),
+                "content" => scapehtml($v['content'])
+            );
+        }
+        echo returnData(2001, '查询成功', $data);
+    } else {
+        echo returnData(2002, '查询错误');
+    }
+}
+/**
+ * 添加文章
+ */
 function articleAdd($model, $data)
 {
-    $ins_data=array(
-        "title"=>$data['title'],
-        "categories"=>$data['categories'],
-        "keywords"=>$data['keywords'],
-        "content"=>htmlspecialchars($data['content'])
+    $ins_data = array(
+        "title" => htmlscape($data['title']),
+        "categories" => htmlscape($data['categories']),
+        "keywords" => htmlscape($data['keywords']),
+        "content" => htmlscape($data['content'])
     );
-    $res=$model->add('article',$ins_data);
-    if($res){
-        echo json_encode('添加成功');
-    }else{
-        echo json_encode('添加失败'); 
+    $res = $model->add('article', $ins_data);
+    if ($res) {
+        echo returnData(2001, '添加成功');
+    } else {
+        echo returnData(2002, '添加失败');
     }
+}
+/**
+ * 删除文章
+ * 根据id进行删除
+ */
+function articleDel($model, $data)
+{
+    $where=array(
+        'id'=>$data['id']
+    );
+    $res = $model->del('article', $where);
+    if ($res) {
+        echo returnData(2001, '删除成功');
+    } else {
+        echo returnData(2002, '删除失败');
+    }
+}
+/**
+ * 修改文章
+ */
+function articleEdit($model,  $data)
+{
+    $res = $model->edit('article', $where, $data);
+    if ($res) {
+        echo returnData(2001, '更新成功');
+    } else {
+        echo returnData(2002, '更新失败');
+    }
+}
+/**
+ * 查询指定文章
+ */
+function articleQuery($model, $where)
+{
+    $res = $model->getOneData('article', $where);
+    if ($res) {
+        echo returnData(2001, '查询成功');
+    } else {
+        echo returnData(2002, '查询失败');
+    }
+}
+/**
+ * html代码转义
+ */
+function htmlscape($data)
+{
+    return htmlspecialchars($data);
+}
+function scapehtml($data)
+{
+    return htmlspecialchars_decode($data);
+}
+/**
+ * 返回数据格式
+ * 2001=>成功
+ * 2002=>错误
+ */
+function returnData($code, $msg, $data = array())
+{
+    $array = array(
+        'code' => $code,
+        'msg' => $msg,
+        'data' => $data
+    );
+    return json_encode($array);
 }
 
 try {
@@ -40,19 +129,19 @@ try {
         //执行方法
         if ($req['fun'] == 'lst') {
             //获取列表
-            echo json_encode('adsf');//测试数据
+            articleLst($model);
         } elseif ($req['fun'] == 'add') {
             //添加数据
-            articleAdd($model,$req);
+            articleAdd($model, $req);
         } elseif ($req['fun'] == 'del') {
             //删除
-
+            articleDel($model,$req);
         } elseif ($req['fun'] == 'edit') {
             //修改
-
+            articleEdit($model,$req);
         } elseif ($req['fun'] == 'query') {
             //查询
-
+            articleQuery($model,$req);
         }
         //markdown文件内容
         if ($req['fun'] == 'mdfile') {
